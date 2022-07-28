98a546e46e6e67823405a839f221ee9b5afc7432
huskyhehe <zhou.he1@northeastern.edu>
2022-07-28 14:20:22 -0700

    Add db connection

--- server/server.js
+++ server/server.js
@@ -1,4 +1,16 @@
-const express = require("express");
+import express from "express";
+
+// detenv
+import dotenv from 'dotenv'
+dotenv.config();
+
+// db
+import connectDb from "./db/conn.js";
+
+// middleware
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+import notFoundMiddleware from "./middleware/not-found.js";
+
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -7,6 +19,19 @@ app.get('/', (req, res) => {
     res.send('Welcome!');
 });
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}.`);
-});
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+
+const start = async() => {
+    try {
+        await connectDb(process.env.ATLAS_URI);
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}.`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
