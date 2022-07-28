bcd7337ba965863fe41b6978a0d81075816bc0c6
huskyhehe <zhou.he1@northeastern.edu>
2022-07-27 22:58:44 -0700

    Add register route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Error from './pages/Error';
+import Register from './pages/Register';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/landing' element={<Landing />} />
+                <Route path='/register' element={<Register />} />
                 <Route path='/error' element={<Error />} />
 
             </Routes>
