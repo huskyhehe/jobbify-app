8cccffb148a2fc280a13eee678daef7f8ffb63c0
huskyhehe <zhou.he1@northeastern.edu>
2022-07-27 17:23:11 -0700

    Add landing page and error page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
+import Error from './pages/Error';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/landing' element={<Landing />} />
+                <Route path='/error' element={<Error />} />
 
             </Routes>
         </BrowserRouter>
