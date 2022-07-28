18993f4e8de6595b5a505c36138ec2ae845569d9
huskyhehe <zhou.he1@northeastern.edu>
2022-07-28 15:15:04 -0700

    Add controllers

---
+++ server/controllers/authController.js
@@ -0,0 +1,11 @@
+export const register = (req, res) => {
+    res.send('register user');
+};
+
+export const login = (req, res) => {
+    res.send('login user');
+};
+
+export const updateUser = (req, res) => {
+    res.send('update user');
+};
