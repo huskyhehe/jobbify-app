e1a388fb083763f1b7924146f0af10589b0f68c6
huskyhehe <zhou.he1@northeastern.edu>
2022-07-28 15:30:17 -0700

    Add validate for email

--- server/models/User.js
+++ server/models/User.js
@@ -11,6 +11,10 @@ const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: [true, 'Please provide email'],
+        validate: {
+            validator: validator.isEmail,
+            message: 'Please provide a valid email',
+        },
         unique: true,
     },
     password: {
