42619afed1c75b81a910156a2448d23a4adce8ee
huskyhehe <zhou.he1@northeastern.edu>
2022-07-28 15:15:28 -0700

    Apply routes in server.js

--- server/server.js
+++ server/server.js
@@ -1,27 +1,35 @@
 import express from "express";
 
-// detenv
+// dotenv
 import dotenv from 'dotenv'
 dotenv.config();
 
 // db
 import connectDb from "./db/conn.js";
 
+// routers
+import authRouter from "./routes/authRoutes.js";
+import jobsRouter from "./routes/jobsRoutes.js";
+
 // middleware
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 
-const app = express();
 
+const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome!');
 });
 
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
-
 
 const start = async() => {
     try {
