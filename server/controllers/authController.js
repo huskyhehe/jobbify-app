85a73eb22a6e42d595d8a84f10b23063e3dcd91b
huskyhehe <zhou.he1@northeastern.edu>
2022-07-29 11:24:24 -0700

    Add jwt

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -15,7 +15,14 @@ export const register = async (req, res) => {
     };
 
     const user = await User.create({ name, email, password });
-    res.status(StatusCodes.OK).json({ user });
+    const token = user.createJWT();
+    res.status(StatusCodes.OK).json({ 
+        user: {
+            name: user.name,
+            email: user.email,
+            location: user.location,    
+        }, 
+        token });
 };
 
 export const login = async (req, res) => {
