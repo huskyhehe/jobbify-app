d9e695d313807357dd6b66b9fc3f0607e165df01
huskyhehe <zhou.he1@northeastern.edu>
2022-07-29 11:59:17 -0700

    Add dashboard route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Error from './pages/Error';
 import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path='/' element={<Dashboard />} />
                 <Route path='/landing' element={<Landing />} />
                 <Route path='/register' element={<Register />} />
                 <Route path='/error' element={<Error />} />
