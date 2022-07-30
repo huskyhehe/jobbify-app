fa288a6b130403f3b8227a1eb02e5f0ff0d37fba
huskyhehe <zhou.he1@northeastern.edu>
2022-07-30 14:03:38 -0700

    Persist user in local storage

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -8,21 +8,25 @@ import {
     REGISTER_USER_SUCCESS,
     REGISTER_USER_ERROR } from './actions';
 
+const token = localStorage.getItem('token');
+const user = localStorage.getItem('user');
+const userLocation = localStorage.getItem('location');
+
 export const initialState = {
     isLoading: false,
     showAlert: false,
     alertText: '',
     alertType: '',
-    user: null,
-    token: null,
-    userLocation: '',
-    jobLocation: ''
+    user: user ? JSON.parse(user) : null,
+    token: token,
+    userLocation: userLocation || '',
+    jobLocation: userLocation || '',
 };
 
 const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(reducer, initialState);  
+    const [state, dispatch] = useReducer(reducer, initialState);
 
     const displayAlert = () => {
         dispatch({
@@ -45,6 +49,11 @@ export const AppProvider = ({ children }) => {
                     location,
                 },
             });
+            addUserToLocalStorage({
+                user,
+                token,
+                location,
+            });
         } catch (error) {
             console.log(error.response);
             dispatch({
@@ -54,6 +63,18 @@ export const AppProvider = ({ children }) => {
         }
         clearAlert();
     };
+
+    const addUserToLocalStorage = ({ user, token, location }) => {
+        localStorage.setItem('user', JSON.stringify(user))
+        localStorage.setItem('token', token)
+        localStorage.setItem('location', location)
+    }
+      
+      const removeUserFromLocalStorage = () => {
+        localStorage.removeItem('token')
+        localStorage.removeItem('user')
+        localStorage.removeItem('location')
+    }
     
     const clearAlert = () => {
         setTimeout(() => {
