6416b73b4a784ecf50ca232ce560a10c6cb6813c
huskyhehe <zhou.he1@northeastern.edu>
2022-07-31 16:23:54 -0700

    Add login

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -6,7 +6,10 @@ import {
     CLEAR_ALERT, 
     REGISTER_USER_BEGIN,
     REGISTER_USER_SUCCESS,
-    REGISTER_USER_ERROR } from './actions';
+    REGISTER_USER_ERROR,
+    LOGIN_USER_BEGIN,
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_ERROR } from './actions';
 
 const token = localStorage.getItem('token');
 const user = localStorage.getItem('user');
@@ -32,6 +35,35 @@ export const AppProvider = ({ children }) => {
         dispatch({
             type: DISPLAY_ALERT,
         });
+        clearAlert();
+    };
+
+    const clearAlert = () => {
+        setTimeout(() => {
+            dispatch({
+                type: CLEAR_ALERT,
+            })
+        }, 3000)
+    };
+
+    const loginUser = async (currentUser) => {
+        dispatch({ type: LOGIN_USER_BEGIN })
+        try {
+            const { data } = await axios.post('/api/v1/auth/login', currentUser);
+            const { user, token, location } = data;
+        
+            dispatch({
+                type: LOGIN_USER_SUCCESS,
+                payload: { user, token, location },
+            });
+        
+            addUserToLocalStorage({ user, token, location })
+        } catch (error) {
+            dispatch({
+                type: LOGIN_USER_ERROR,
+                payload: { msg: error.response.data.msg },
+            });
+        };
         clearAlert();
     };
 
@@ -76,14 +108,6 @@ export const AppProvider = ({ children }) => {
         localStorage.removeItem('location')
     }
     
-    const clearAlert = () => {
-        setTimeout(() => {
-            dispatch({
-                type: CLEAR_ALERT,
-            })
-        }, 3000)
-    };
-
     return (
         <AppContext.Provider
             value={{
