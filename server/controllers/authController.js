b5edf0f4041999cc094d420f336ef77d69ae56a9
huskyhehe <zhou.he1@northeastern.edu>
2022-08-02 20:41:47 -0700

    Refactor setup

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -16,11 +16,11 @@ export const register = async (req, res) => {
 
     const user = await User.create({ name, email, password });
     const token = user.createJWT();
-    res.status(StatusCodes.OK).json({ 
+    res.status(StatusCodes.CREATED).json({ 
         user: {
             name: user.name,
             email: user.email,
-            location: user.location,    
+            location: user.location  
         }, 
         token,
         location: user.location
