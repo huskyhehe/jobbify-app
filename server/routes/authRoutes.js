06432bb5acbe0b3a300624a03dec06b0f3aef822
huskyhehe <zhou.he1@northeastern.edu>
2022-08-03 15:07:53 -0700

    Import auth.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,9 +2,10 @@ import express from 'express';
 const authRouter = express.Router();
 
 import { register, login, updateUser } from "../controllers/authController.js";
+import authenticateUser from '../middleware/auth.js';
 
 authRouter.route('/register').post(register);
 authRouter.route('/login').post(login);
-authRouter.route('/updateUser').patch(updateUser);
+authRouter.route('/updateUser').patch(authenticateUser, updateUser);
 
 export default authRouter;
