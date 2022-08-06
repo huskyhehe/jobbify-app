184aeada88a3ae5b6bfba79027882238126ac957
huskyhehe <zhou.he1@northeastern.edu>
2022-08-06 14:50:24 -0700

    Update updateJob function

--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -22,13 +22,36 @@ export const getAllJobs = async (req, res) => {
       .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
 };
 
+
 export const deleteJob = (req, res) => {
     res.send('delete job');
 };
 
-export const updateJob = (req, res) => {
-    res.send('update jobs');
+
+export const updateJob = async (req, res) => {
+    const { id: jobId } = req.params;
+    const { company, position } = req.body;
+  
+    if (!position || !company) {
+      throw new BadRequestError('Please provide all values');
+    };
+
+    const job = await Job.findOne({ _id: jobId }); 
+    if (!job) {
+      throw new NotFoundError(`No job with id :${jobId}`)
+    };
+
+    // check permissions 
+    // checkPermissions(req.user, job.createdBy)
+  
+    const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+  
+    res.status(StatusCodes.OK).json({ updatedJob });
 };
+
 
 export const showStats = (req, res) => {
     res.send('show stats');
