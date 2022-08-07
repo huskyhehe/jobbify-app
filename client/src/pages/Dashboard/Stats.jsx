1b1931b9e33369258a396f195353939a761df851
huskyhehe <zhou.he1@northeastern.edu>
2022-08-07 16:44:22 -0700

    Add Stats section

--- client/src/pages/Dashboard/Stats.jsx
+++ client/src/pages/Dashboard/Stats.jsx
@@ -1,6 +1,23 @@
+import { useEffect } from "react";
+import StatsContainer from "../../components/StatsContainer";
+import { useAppContext } from "../../context/appContext";
+
 function Stats() {
+
+    const { showStats, isLoading } = useAppContext();
+
+    useEffect(() => {
+        showStats();
+    }, []);
+
+    if (isLoading) {
+        return <div className='loading loading-center'></div>
+    };
+      
     return ( 
-        <h1>Stats</h1>
+        <>
+            <StatsContainer />
+        </>
     );
 }
 
