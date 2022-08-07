fa5c47a6cc59a974fe0f803e52b310ce46438b24
huskyhehe <zhou.he1@northeastern.edu>
2022-08-07 15:27:47 -0700

    Add populate.js

--- server/populate.js
+++ server/populate.js
@@ -8,7 +8,7 @@ import Job from './models/Job.js';
 
 const start = async () => {
   try {
-        await connectDB(process.env.MONGO_URL);
+        await connectDB(process.env.ATLAS_URI);
         await Job.deleteMany();
         const jsonProducts = JSON.parse(
             await readFile(new URL('./mock-data.json', import.meta.url))
