9f82ac147d4d9e1987d4ddcff26b350e3f840ade
huskyhehe <zhou.he1@northeastern.edu>
2022-08-08 19:21:59 -0700

    Add rateLimiter in authRoutes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,13 @@
 import express from 'express';
 const authRouter = express.Router();
 
+import rateLimiter from 'express-rate-limit';
+const apiLimiter = rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 10,
+    message: 'Too many requests from this IP, please try again after 15 minutes'
+});
+
 import { register, login, updateUser } from "../controllers/authController.js";
 import authenticateUser from '../middleware/auth.js';
 
